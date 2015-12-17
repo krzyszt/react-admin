b532e3535ecfb5fa37a2675c735e52dfbca2de71
Krzysztof Kaminski <krzysztof.teonte@gmail.com>
2015-12-17 07:48:50 +0000

    react router

--- src/main.js
+++ src/main.js
@@ -1,50 +1,12 @@
-$ = jQuery = require('jquery');
+
 
 var React = require('react');
+var Router = require('react-router');
+var routes = require('./routes');
 
-var Home = require('./components/homePage');
-var About = require('./components/about/aboutPage');
-var Authors = require('./components/authors/authorPage');
-var Header = require('./components/common/header');
-
-
-(function(win){
+Router.run(routes, function(Handler) {
     "use strict";
 
-    var App = React.createClass({
-        render: function(){
-            var Child;
+    React.render(<Handler />, document.getElementById('app'));
 
-
-            switch (this.props.route) {
-                case 'about':
-                    Child = About;
-                    break;
-                case 'authors':
-                    Child = Authors;
-                    break;
-                default:
-                    Child = Home;
-
-            }
-
-            return (
-                <div>
-                    <Header />
-                    <Child />
-                </div>
-            );
-        }
-    });
-
-    function render() {
-        var route = win.location.hash.substr(1);
-        React.render(<App route={route} />, document.getElementById('app'));
-    }
-
-    win.addEventListener('hashchange', render);
-    render();
-
-
-
-})(window);
+});
